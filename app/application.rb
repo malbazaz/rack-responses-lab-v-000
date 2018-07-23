2dfa1bf529257de1914e4b200db8ee6c88b4c076
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-23 14:22:45 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,12 +5,13 @@ class Application
     resp = Rack::Response.new
 
     noon = Time.new(2010,01,01, 12,0,0)
+    binding.pry
     if noon.hour < Time.now.hour
       resp.write("Good Morning")
     else
       resp.write("Good Afternoon")
     end
-
+    resp.finish
   end
 
 
