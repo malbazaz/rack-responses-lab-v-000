646ffde01a61750611ef4efea2d7fa218774f520
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-23 14:18:53 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
   def call(env)
     resp = Rack::Response.new
     binding.pry
-    noon = Time.new
+    noon = Time.new(now.year, now.month, now.day, 12,0,0, now.zone)
     noon.hour = 12
 
   end
