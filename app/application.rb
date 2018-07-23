777bf64e2bfc3a48388103d6f622e1242cfa0731
Mo Al <malbazaz@mba2018.hbs.edu>
2018-07-23 14:10:48 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -2,10 +2,11 @@ require 'pry'
 class Application
 
   def call(env)
-    resp = Rack::Response.new 
+    resp = Rack::Response.new
     noon = Time.new
     noon.hour = 12
-    if 
+    binding.pry 
+  end 
 
 
-end 
+end
